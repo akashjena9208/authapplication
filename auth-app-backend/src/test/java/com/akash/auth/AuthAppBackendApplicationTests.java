7eb44dcc21ee370a30ebc923468b58403f57f97e
Akash Jena <akashjena9208@gmail.com>
2025-11-16 17:52:56 +0530

    Initial commit: Full-stack app (Spring Boot + React)

---
+++ auth-app-backend/src/test/java/com/akash/auth/AuthAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
