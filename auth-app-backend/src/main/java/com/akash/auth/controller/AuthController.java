410eb3f28a0036531f9a329962c13f325c13e79a
Akash Jena <akashjena9208@gmail.com>
2026-01-12 23:56:53 +0530

    refresh token generate and store inside db

--- auth-app-backend/src/main/java/com/akash/auth/controller/AuthController.java
+++ auth-app-backend/src/main/java/com/akash/auth/controller/AuthController.java
@@ -3,8 +3,10 @@ package com.akash.auth.controller;
 import com.akash.auth.dto.LoginRequest;
 import com.akash.auth.dto.TokenResponse;
 import com.akash.auth.dto.UserDto;
+import com.akash.auth.entity.RefreshToken;
 import com.akash.auth.entity.User;
 import com.akash.auth.mapper.UserMapper;
+import com.akash.auth.repository.RefreshTokenRepository;
 import com.akash.auth.repository.UserRepository;
 import com.akash.auth.service.AuthService;
 import com.akash.auth.util.JwtUtil;
@@ -32,6 +34,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
+import java.util.UUID;
+
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -42,6 +47,8 @@ public class AuthController {
     private final UserRepository userRepository;
     private final AuthenticationManager authenticationManager; //this guy check username pw
     private final JwtUtil jwtUtil;
+    private final RefreshTokenRepository refreshTokenRepository;
+
 
 
     @PostMapping("/register")
@@ -54,19 +61,37 @@ public class AuthController {
     public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest loginRequest, HttpServletResponse response) {
         // authenticate
         Authentication authenticate = authenticate(loginRequest);
+        // Fetch user
         User user = userRepository.findByEmail(loginRequest.email()).orElseThrow(() -> new BadCredentialsException("Invalid Username or Password"));
         if (!user.isEnabled()) {
             throw new DisabledException("User is disabled");
         }
 
-        //access token--generate
-        String accessToken = jwtUtil.generateAccessToken(user);
+        //Create Refresh Token (DB)
+        String jti = UUID.randomUUID().toString();
+        var newrefreshTokenOb = RefreshToken.builder()
+                .jti(jti)
+                .user(user)
+                .createdAt(Instant.now())
+                .expiresAt(Instant.now().plusSeconds(jwtUtil.getRefreshTtlSeconds()))
+                .revoked(false)
+                .build();
+        //refresh token information save
+        refreshTokenRepository.save(newrefreshTokenOb);
 
-        TokenResponse tokenResponse = TokenResponse.of(accessToken, "", jwtUtil.getAccessTtlSeconds(),UserMapper.toDto(user));
+        //Generate Access Token
+        String accessToken = jwtUtil.generateAccessToken(user);
+        String refreshToken = jwtUtil.generateRefreshToken(user, newrefreshTokenOb.getJti());
+
+
+
+        //Response
+        TokenResponse tokenResponse = TokenResponse.of(accessToken, refreshToken, jwtUtil.getAccessTtlSeconds(),UserMapper.toDto(user));
         return ResponseEntity.ok(tokenResponse);
 
     }
 
+    //Generate Authentication
     private Authentication authenticate(LoginRequest loginRequest) {
         try {
 
---
+++ auth-app-backend/src/main/java/com/akash/auth/repository/RefreshTokenRepository.java
@@ -0,0 +1,9 @@
+package com.akash.auth.repository;
+
+import com.akash.auth.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+}
